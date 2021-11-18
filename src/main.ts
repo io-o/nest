2d17366b5ac813e786f913b8dcc92b89c030b05b
andrew Q <mygao666@gmail.com>
2021-11-18 10:58:05 +0800

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,27 @@
  * @Author: Andrew Q
  * @Date: 2021-11-09 17:09:49
  * @LastEditors: Andrew Q
- * @LastEditTime: 2021-11-09 17:09:49
+ * @LastEditTime: 2021-11-17 15:08:52
  * @Description:
  */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const options = new DocumentBuilder()
+    .setTitle('api')
+    .setDescription('api')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
